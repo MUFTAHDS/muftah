0eedd639815be7544716a99c126b574a01cf3fd3
MUFTAHDSا <balraslymftah@gmail.com>
2021-11-23 18:52:16 +0300

    f

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import logo from './oli2.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Appy'>
+        <img src="../public/imagas/logo192-iros.jpg" alt="" />
+        <h1 className="title red">  بسم الله الرحمن الرحيم </ h1>
+        <h1 className="title red"> مرحباً بك </ h1>
+
+        <video width="320" height="240" عناصر تحكم />
+
+        <source src="myVideo.mp4" type="video / mp4" />
+
+
+        <br />
+
+        <img src={logo} />
+
+        <br />
+        <img src="./ola1.jpg" />
+
+      </div>
+      <video width="320" height="240" عناصر تحكم >
+
+        <source src="myVideo.mp4" type="video / mp4" />
+
+      </video>
     </div>
   );
 }
